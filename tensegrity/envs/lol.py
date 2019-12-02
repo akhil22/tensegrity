f669b7707b1fab2c4749c2a9038b3ef3ce4ef745
akhil Nagariya <akhil.nagariya@gmail.com>
2019-12-02 14:38:38 -0600

    added two legs

--- tensegrity/envs/lol.py
+++ tensegrity/envs/lol.py
@@ -4,25 +4,36 @@ from gym.envs.mujoco import mujoco_env as mj
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.common.vec_env import SubprocVecEnv
+#from stable_baselines.common import make_vec_env
 from stable_baselines import PPO2, TRPO
 from stable_baselines.bench import Monitor
 import os
 import matplotlib.pyplot as plt
 def main():
-    train = True
-    n_cpu = 16
+    train = False
+    cont_train = False
+    n_cpu = 8
     env = SubprocVecEnv([lambda: gym.make('tensegrity:TensLeg-v0') for i in range(n_cpu)])
     #env= gym.make('tensegrity:TensLeg-v0')
     #env = DummyVecEnv([lambda: env])
-    env = SubprocVecEnv([lambda: gym.make('tensegrity:TensLeg-v0') for i in range(n_cpu)])
+   # env = SubprocVecEnv([lambda: gym.make('HalfCheetah-v2') for i in range(n_cpu)])
+   # env = SubprocVecEnv([lambda: gym.make('CartPole-v1') for i in range(n_cpu)])
     
-    model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log='./results')
+    model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log='./results', cliprange=0.07, learning_rate=0.000000025, ent_coef=0.0000001)
     if train:
-        model.learn(total_timesteps=750000)
-        model.save("tppo2_cartpole")
+        if cont_train:
+            model.load_parameters(load_path_or_dict="tppo2_cartpole_fall", exact_match=True)
+        model.learn(total_timesteps=2750000)
+        print('saving model')
+        model.save("tppo2_cartpole_fall2")
+       # del model
     else:
-        model.load("tppo2_cartpole")
+        print('loading model')
+        model = PPO2.load("tppo2_cartpole_fall2")
+    #modeload("tppo2_cartpole")
     env2 = gym.make('tensegrity:TensLeg-v0')
+   # env2 = gym.make('HalfCheetah-v2')
+   # env2 = gym.make('CartPole-v1')
     obs=env2.reset()
     i = 0
     lin_vel  =[]
@@ -30,9 +41,10 @@ def main():
         #action, _states = model.predict(obs) 
         action = env2.action_space.sample()
         obs, rewards, dones, info = env2.step(action)
-        print(action)
-        lin_vel.append(info['reward_linvel'])
-        print(info)
+      #  print(action)
+        lin_vel.append(rewards)
+      #  print('sdata')
+        print(info['reward_linvel'], info['height_rew'], info['reward_impact'], info['sd'])
         i = i+1
         env2.render(mode="human")
     plt.plot(lin_vel)
--- tensegrity/envs/tensegrity_leg.py
+++ tensegrity/envs/tensegrity_leg.py
@@ -29,36 +29,45 @@ class TensLeg(mujoco_env.MujocoEnv, utils.EzPickle):
         pos_before = self._mass_center(self.model, self.sim)
         self.do_simulation(action, self.frame_skip)
         pos_after = self._mass_center(self.model, self.sim)
+        vel = (pos_after - pos_before)/self.dt
 #         alive_bonus = 5.0
         data = self.sim.data
 #        height = self.named.xipos['torso1', 'x']
 #        foot   = self.named.xipos['foot', 'x']
 #        hop_cost = 400*(height-foot)
         lin_vel_x = self.sim.data.get_body_xvelp("torso1")
-        hip_height_cost = 0*self.sim.data.get_body_xpos("Lower_leg")[0]
-        lin_vel_x_low = self.sim.data.get_body_xvelp("Lower_leg")[1]
-        hip_height_cost2 = 30*self.sim.data.get_body_xpos("body_bar00L")[0]
-        hip_height_cost3 = self.sim.data.get_body_xpos("torso1")[0]
+        hip_height_cost = 0*self.sim.data.get_body_xpos("Lower_leg_left")[0]
+       # hip_height_cost = 0*self.sim.data.get_body_xpos("Lower_leg_left")[0]
+        #lin_vel_x_low = self.sim.data.get_body_xvelp("Lower_leg_left")[1]
+        lin_vel_x_low = self.sim.data.get_body_xvelp("Lower_leg_left")[1] + self.sim.data.get_body_xvelp("Lower_leg_right")[1]
+        hip_height_cost2 = self.sim.data.get_body_xpos("body_bar00L")[0] + self.sim.data.get_body_xpos("body_bar00R")[0]
+        hip_height_cost3 = self.sim.data.get_body_xpos("body_bar00L")[0] + self.sim.data.get_body_xpos("body_bar00R")[0]
+       # hip_height_cost3 = self.sim.data.get_body_xpos("torso1")[0]
         #hip_height_cost = hip_height_cost*hip_height_cost + hip_height_cost2*hip_height_cost2
-        hip_height_cost = hip_height_cost + hip_height_cost2 + hip_height_cost3
-        jcost = self.sim.data.get_joint_qvel("t2bar00L")*2
+        hip_height_cost = hip_height_cost3 + hip_height_cost2 + hip_height_cost
+        hip_height_cost = self.sim.data.get_body_xpos("torso1")[0]
+        jcost1 = self.sim.data.get_joint_qvel("t2bar00L")
+        jcost2 = self.sim.data.get_joint_qvel("t2bar00R")
        # hip_height_cost = np.square(2-data.xipos[1, 0])
 #         hip_height_cost = 0
        # lin_vel_cost = 20 * (pos_after[2] - pos_before[2]) / self.dt
-        lin_vel_cost = (5*lin_vel_x[1] + 0*lin_vel_x[0]*lin_vel_x[0] -  5*lin_vel_x_low)
+        sensor_data = self.sim.data.sensordata
+        lin_vel_cost = (0*vel + lin_vel_x[1] + 0*lin_vel_x[0]*lin_vel_x[0] +  0*lin_vel_x_low)
         quad_ctrl_cost = 0.01 * np.square(data.ctrl).sum()
-        quad_impact_cost = .5e-9 * np.square(data.cfrc_ext).sum()
+        quad_impact_cost = .5e-1 * np.square(data.cfrc_ext).sum()
         quad_impact_cost = min(quad_impact_cost, 10)
-        reward = 0 * lin_vel_cost - quad_ctrl_cost - quad_impact_cost - 10 * hip_height_cost + jcost*jcost*0
-        reward = lin_vel_cost*hip_height_cost
+       # reward = 0 * lin_vel_cost - quad_ctrl_cost - quad_impact_cost - 10 * hip_height_cost + jcost*jcost*0
+        reward = lin_vel_cost*50 + hip_height_cost - (sensor_data[2] > 0)*50 - (sensor_data[4] > 0)*50 + sensor_data[0]*0 - quad_impact_cost - (sensor_data[8] > 0)*50 - (sensor_data[10] > 0)*50 + sensor_data[6]*0 + 0.5*(jcost1*jcost2 < 0)
      #   reward = 5 * lin_vel_cost - quad_ctrl_cost - quad_impact_cost
+        #reward = lin_vel_cost*100
         qpos = self.sim.data.qpos
         ob = self._get_obs()
+       # done = sensor_data[2] > 0
         done = False
         DOF = self.model.body_dofnum
 #         done = bool((qpos[2] < 1.0) or (qpos[2] > 2.0))
-        return ob, reward, done, dict(reward_linvel=hip_height_cost, reward_quadctrl=-quad_ctrl_cost, 
-                                      reward_impact=-quad_impact_cost, joint_cost = jcost*jcost, DOF = DOF)
+        return ob, reward, done, dict(reward_linvel=vel*100, height_rew=hip_height_cost, 
+                                      reward_impact=20*(jcost1*jcost2 < 0), joint_cost = 20*jcost1*jcost2, DOF = DOF, sd = sensor_data)
 
 #     def _get_obs(self):
 #         return np.concatenate([
